66f55b88c1ccf18b9618c92ce1da6157357010f6
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-01-02 13:32:05 -0500

    created window class

---
+++ assets/src/HomePage.js
@@ -0,0 +1,5 @@
+async function initHomePage(){
+    let fileInputWindow = new Window("input file here", "fileInput", "File Input")
+    fileInputWindow.generate()
+    fileInputWindow.render()
+}
---
+++ assets/src/window.js
@@ -0,0 +1,56 @@
+class Window{
+    constructor(contents, className, title){
+        this.contents = contents
+        this.className = className
+        this.title = title
+    }
+
+    generate(){
+        this.window = document.createElement('div')
+        this.window.className = this.className + ' window'
+        this.#createTitleBar()
+        this.#createBody()
+    }
+
+    render(){
+        document.body.appendChild(this.window)
+    }
+
+    addFooter(content){
+        let footer = document.createElement('section')
+        footer.className = 'field-row'
+        footer.style.justifyContent = 'flex-end'
+        footer.innerHTML = content
+        this.window.getElementsByClassName('window-body')[0].appendChild(footer)
+    }
+
+    #createTitleBar(){
+        let titleBar = document.createElement('div')
+        titleBar.className = 'title-bar'
+
+        //create title bar text
+        let titleBarText = document.createElement('div')
+        titleBarText.className = 'title-bar-text'
+        titleBarText.innerHTML = this.title
+        titleBar.appendChild(titleBarText)
+
+        //create title bar controls
+        let titleBarControls = document.createElement('div')
+        titleBarControls.className = 'title-bar-controls'
+        titleBarControls.innerHTML = `
+            <button aria-label="Minimize"></button>
+            <button aria-label="Maximize"></button>
+            <button aria-label="Close"></button>
+        `
+        titleBar.appendChild(titleBarControls)
+
+        this.window.appendChild(titleBar)
+    }
+
+    #createBody(){
+        let body = document.createElement('div')
+        body.className = 'window-body'
+        body.innerHTML = this.contents
+        this.window.appendChild(body)
+    }
+}
